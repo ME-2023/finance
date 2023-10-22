d666f0e99edf1cde114355d9386863aac4b65a52
ME-2023 <99931738+ME-2023@users.noreply.github.com>
2023-10-22 07:56:22 -0300

    Create indicators.py

---
+++ pages/indicators.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.write("""
+    ## Analized Indicators
+    
+    In this section we present some of the relationships studied.
+                
+    """)
