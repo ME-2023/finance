014e30115f4b77cae5ecbf1e86c34a76bacc3f8d
ME-2023 <99931738+ME-2023@users.noreply.github.com>
2023-10-22 07:03:35 -0300

    Update home.py

--- home.py
+++ home.py
@@ -11,6 +11,19 @@ def run():
     )
 
     st.write("# MEINPS Finance")
+    st.write("# Data Science Web App")
+    st.write("""
+    MEINPS Finance is a project that focuses its efforts on the study of collective economic behavior 
+    through AI and Data Science. It aims to integrate knowledge from different domains of the 
+    human being, applied to collective human behavior.
+    
+    MEINPS considers that the most evolved aspects of the human being are defined by collective 
+    processes and that these have a digital extension. The human being at the beginning of the 
+    21st century has a Digital Collective Behavior.
+    
+    The ontological foundation of MEINPS's actions is found in cooperative and empathetic work 
+    between people; the epistemological basis is found in the scientific method.
+    """)
 
     st.sidebar.success("Select a demo above.")
 
