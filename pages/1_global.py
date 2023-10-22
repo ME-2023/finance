5590d037f29222e35d53181e5a1eb730db7d2182
ME-2023 <99931738+ME-2023@users.noreply.github.com>
2023-10-22 10:44:06 +0000

    Global current_date

--- pages/1_global.py
+++ pages/1_global.py
@@ -2,7 +2,11 @@ import streamlit as st
 import pandas as pd
 import requests
 import json
+from datetime import datetime
 # import yfinance as yf
+
+# Current date
+current_date = datetime.today().strftime('%Y/%m/%d')
 
 st.write("""
     ## Composición Merval
@@ -22,11 +26,12 @@ ticker_symbol = st.selectbox(
     )
 
 st.write(f"You selected {ticker_symbol}")
+st.write(f'From: 2018/01/09 to {current_date}')
 
 try:
     # Authentication
     # Pass your API key in the query string like follows:
-    api_url = f'https://api.polygon.io/v2/aggs/ticker/{ticker_symbol}/range/1/day/2018-01-09/2023-10-15?apiKey=LOpAdL70TOrSPQh9t3UaYAFpr2Dq34po'
+    api_url = f'https://api.polygon.io/v2/aggs/ticker/{ticker_symbol}/range/1/day/2018-01-09/{current_date}?apiKey=LOpAdL70TOrSPQh9t3UaYAFpr2Dq34po'
 
     # Login, para generar un token
     # Un token es un identificador único que se le da al usuario para poder realizar las solicitudes necesarias
