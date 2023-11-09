20c4433bc9b5d99a4d5a57a439798a7399b3ce7b
ME-2023 <99931738+ME-2023@users.noreply.github.com>
2023-11-09 01:34:14 +0000

    ma-X selected

--- pages/4_medias_moviles.py
+++ pages/4_medias_moviles.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
+import requests
+import json
+from datetime import datetime
 
 st.write("""
 # Medias Móviles Trading App
@@ -12,11 +15,13 @@ Esta app predice los resultados de una estrategia de trading!
 st.sidebar.header('Modificar parámetros')
 
 def user_input_features():
-    moving_avg = st.sidebar.slider('Media móvil', 10, 100, 10, 10)
+    global data
+
+    moving_avg = st.sidebar.slider('Media móvil', 10, 100, 50, 10)
     major_trend = st.sidebar.radio('Tendencia de LP', ('bull', 'bear'))
 
     data = {
-        'Media móvil': moving_avg,
+        'Media movil': moving_avg,
         'Tendencia de LP': major_trend,
 
     }
@@ -25,8 +30,26 @@ def user_input_features():
 
 df = user_input_features()
 
+# Read the CSV file
+df_apple = pd.read_csv('aapl_1d.csv')
+
+# Now 'df_apple' is a DataFrame containing the data from 'aapl_1d.csv'
+
+
 st.subheader('Parámetros ingresados por el usuario')
 st.write(df)
+
+#st.write(df_apple)
+
+st.write("""
+## Precio de cierre (1d)
+""")
+
+ma = f"ma-{data['Media movil']}"
+
+st.line_chart(df_apple[['close', ma]])
+
+
 
 """
 iris = datasets.load_iris()
