dea3ad9cb14bcc845646d255f1207628f46685f5
ME-2023 <99931738+ME-2023@users.noreply.github.com>
2023-11-10 12:48:39 +0000

    Pyplot

--- pages/4_medias_moviles.py
+++ pages/4_medias_moviles.py
@@ -5,6 +5,9 @@ from sklearn.ensemble import RandomForestClassifier
 import requests
 import json
 from datetime import datetime
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
 st.write("""
 # Medias Móviles Trading App
@@ -16,14 +19,31 @@ st.sidebar.header('Modificar parámetros')
 
 def user_input_features():
     global data
+    global ticker
 
     moving_avg = st.sidebar.slider('Media móvil', 10, 100, 50, 10)
-    major_trend = st.sidebar.radio('Tendencia de LP', ('bull', 'bear'))
+    major_trend = st.sidebar.radio('Tendencia de LP', ('bull market', 'bear market'))
+    ticker_symbol = st.sidebar.selectbox(
+    "Seleccionar ticker",
+    ["AAPL", "BAC", "AXP", 
+    "CVX", "KO",],
+    )
+
+    if ticker_symbol == 'AAPL':
+        ticker = 'aapl_1d_3.csv'
+    if ticker_symbol == 'BAC':
+        ticker = 'bac_1d.csv'
+    if ticker_symbol == 'AXP':
+        ticker = 'axp_1d.csv'
+    if ticker_symbol == 'CVX':
+        ticker = 'cvx_1d.csv'
+    if ticker_symbol == 'KO':
+        ticker = 'ko_1d.csv'
 
     data = {
+        'Ticker': ticker_symbol,
         'Media movil': moving_avg,
         'Tendencia de LP': major_trend,
-
     }
     features = pd.DataFrame(data, index=[0])
     return features
@@ -31,7 +51,8 @@ def user_input_features():
 df = user_input_features()
 
 # Read the CSV file
-df_apple = pd.read_csv('aapl_1d.csv')
+#df_apple = pd.read_csv('aapl_1d.csv')
+df_selected = pd.read_csv(ticker)
 
 # Now 'df_apple' is a DataFrame containing the data from 'aapl_1d.csv'
 
@@ -40,6 +61,7 @@ st.subheader('Parámetros ingresados por el usuario')
 st.write(df)
 
 #st.write(df_apple)
+st.write(df_selected.head(5))
 
 st.write("""
 ## Precio de cierre (1d)
@@ -47,22 +69,35 @@ st.write("""
 
 ma = f"ma-{data['Media movil']}"
 
-st.line_chart(df_apple[['close', ma]])
+# st.line_chart(df_apple[['close', ma]])
+
+st.line_chart(df_selected[['close', ma]])
+
+st.subheader('Análisis en el período seleccionado')
+
+# Assuming apple_daily['days_to_10'] is your data
+data2 = df_selected['days_to_10']
+
+# Create a histogram
+sns.histplot(data2, kde=False, bins=int(np.sqrt(1454)))
+
+# Use pyplot to generate the plot
+plt.show()
+
+# Use Streamlit's pyplot functionality to display the plot
+st.pyplot(plt)
+
+# iris = datasets.load_iris()
+#X = iris.data
+#Y = iris.target
+
+#clf = RandomForestClassifier()
+#clf.fit(X, Y)
+
+#prediction = clf.predict(df)
+#prediction_proba = clf.predict_proba(df)
 
 
-
-"""
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
-
-clf = RandomForestClassifier()
-clf.fit(X, Y)
-
-prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
-
-"""
 
 st.subheader('Class labels and their corresponding index number')
 #st.write(iris.target_names)
