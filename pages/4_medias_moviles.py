1b5bdc3d780556aceb3e8a48640955cc0be90039
ME-2023 <99931738+ME-2023@users.noreply.github.com>
2023-11-14 18:49:37 +0000

    x

--- pages/4_medias_moviles.py
+++ pages/4_medias_moviles.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 import requests
 import json
 from datetime import datetime
-import seaborn as sns
+#import seaborn as sns
 #import matplotlib.pyplot as plt
 import numpy as np
 
